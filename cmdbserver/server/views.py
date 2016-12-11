e21ca307a96c95e6c9e7bcb6782fe3bb138d7470
yxxhero <526169407@qq.com>
2016-12-12 05:56:05 +0800

    add web

--- cmdbserver/server/views.py
+++ cmdbserver/server/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.template.context_processors import request
+from django.http.response import HttpResponse
+from django.shortcuts import render_to_response
+import json
 # Create your views here.
+def index(request):
+    return render_to_response("index.html")    
+def hostinfolist(request):
+    hostinfo={"aaData":{"id": "id", "ip": "ip", "minionid": "minionid", "system": "system", "project": "project","location":"location","services":"services","status":"status","operat":"operation"}}
+    return HttpResponse(json.dumps(hostinfo))
