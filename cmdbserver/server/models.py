6078baaf13e1a537259b29d8bcbc04b3d8825a37
root <root@bogon.(none)>
2016-12-13 22:03:22 +0800

    add userinfo table

--- cmdbserver/server/models.py
+++ cmdbserver/server/models.py
@@ -3,3 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class userinfo(models.Model):
+    Name=models.CharField(max_length=30)
+    Email=models.EmailField()
+
