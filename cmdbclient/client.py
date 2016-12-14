39a8b637dc82a346d6f4354e08f02461fac13cd7
yuchaoshui <yuchaoshui@yeah.net>
2016-12-14 14:19:17 +0800

    modify client.py

--- cmdbclient/client.py
+++ cmdbclient/client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 # coding:utf-8
-import os,sys
+import os,sys,commands
 import psutil
 import platform
 import socket
@@ -12,8 +12,23 @@ class system_info(object):
 		self.hostname = ''
 		self.ip_dict = {}
 		self.cpu_info = {}
+		self.kernel_info = ''
+		self.system_name = ''
+		self.architecture = ''
 		self.process_info = {}
 		self.mem_info = {}
+
+	def get_kernel_info(self):
+		self.kernel_info = commands.getstatusoutput("uname -r")[1]
+		return self.kernel_info
+
+	def get_system_name(self):
+		self.system_name = commands.getstatusoutput("uname -o")[1]
+		return self.system_name
+	
+	def get_architecture(self):
+		self.architecture= commands.getstatusoutput("uname -m")[1]
+		return self.architecture
 
 	def get_mem_info(self):
 		self.mem_info['total'] = psutil.virtual_memory().total/1024/1024
@@ -58,10 +73,17 @@ class system_info(object):
 		self.system_info['ip_dict'] = self.get_ip_dict()
 		self.system_info['cpu_info'] = self.get_cpu_info()
 		self.system_info['mem_info'] = self.get_mem_info()
+		self.system_info['kernel_info'] = self.get_kernel_info()
+		self.system_info['system_name'] = self.get_system_name()
+		self.system_info['architecture'] = self.get_architecture()
 		return self.system_info
 
 if __name__ == '__main__':
+	print
+	print
 	client = system_info()
 	for key,item in  client.get_system_info().items():
 		print key,"\t----->",item
 
+	print
+	print
